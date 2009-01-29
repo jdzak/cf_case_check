5dca5699b8c88ef907b30fb4b173fc0310f9ddd4
John Dzak <johndzak@reserved-dhcp-165-124-223-105.nubic.northwestern.edu>
2009-01-29 15:13:02 -0600

    Fix misspelled directory names.

--- spec/coldfusion_8_configuration_spec.rb
+++ spec/coldfusion_8_configuration_spec.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), %w[spec_helper])
 
 describe CaseCheck::Coldfusion8Configuration do
   before do
-    @cf_root = "/tmp/cfm_case_check/"
+    @cf_root = "/tmp/cf_case_check/"
     
     neo_runtime = {
       :filename => "/tmp/cf_case_check/lib/neo-runtime.xml",
@@ -11,9 +11,9 @@ describe CaseCheck::Coldfusion8Configuration do
       "<wddxPacket version='1.0'>        
         <data>
           <struct type='coldfusion.server.ConfigMap'>
-            <var name='/WEB-INF/customtags1207698058868'><string>/tmp/cfm_case_check/customtags</string></var>
-            <var name='/WEB-INF/customtags1207758745211'><string>/tmp/cfm_case_check/cfcs</string></var>
-            <var name='/WEB-INF/customtags1207773084805'><string>/tmp/cfm_case_check/misc</string></var>
+            <var name='/WEB-INF/customtags1207698058868'><string>/tmp/cf_case_check/customtags</string></var>
+            <var name='/WEB-INF/customtags1207758745211'><string>/tmp/cf_case_check/cfcs</string></var>
+            <var name='/WEB-INF/customtags1207773084805'><string>/tmp/cf_case_check/misc</string></var>
           </struct>
         </data>
       </wddxPacket>"
@@ -43,21 +43,21 @@ describe CaseCheck::Coldfusion8Configuration do
   end
   
   it "should read customtag directories from neo-runtime.xml" do
-    touch('/tmp/cfm_case_check/customtags/foo.cfm')
-    touch('/tmp/cfm_case_check/misc/bar.cfm')
+    touch('/tmp/cf_case_check/customtags/foo.cfm')
+    touch('/tmp/cf_case_check/misc/bar.cfm')
     
     build_config
     
-    CaseCheck::CustomTag.directories.should == %w(/tmp/cfm_case_check/customtags /tmp/cfm_case_check/misc)    
+    CaseCheck::CustomTag.directories.should == %w(/tmp/cf_case_check/customtags /tmp/cf_case_check/misc)    
   end
   
   it "should read cfc directories from neo-runtime.xml" do
-    touch('/tmp/cfm_case_check/cfcs/bar.cfc')
-    touch('/tmp/cfm_case_check/misc/foo.cfc')
+    touch('/tmp/cf_case_check/cfcs/bar.cfc')
+    touch('/tmp/cf_case_check/misc/foo.cfc')
     
     build_config
     
-    CaseCheck::CustomTag.directories.should == %w(/tmp/cfm_case_check/cfcs /tmp/cfm_case_check/customtags)
+    CaseCheck::CustomTag.directories.should == %w(/tmp/cf_case_check/cfcs /tmp/cf_case_check/customtags)
   end
   
 end
